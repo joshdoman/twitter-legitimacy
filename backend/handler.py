1dde6552f026ea292d3f677f0a54d999a3003bc5
Josh <jsdoman97@gmail.com>
2021-12-29 13:28:08 -0500

    s3 caching, increase lambda timeout to 30 sec

--- backend/handler.py
+++ backend/handler.py
@@ -3,15 +3,20 @@ import json
 import time
 import os
 
+from botocore.exceptions import ClientError
+
 from functions.UserAPI import get_user_info
 from functions.FollowersAPI import get_followers_for_id, get_following_for_id
 
 # Import DynamoDB
 dynamodb = boto3.client('dynamodb')
+s3 = boto3.client('s3')
 
 # Import Environment Variables
 followersTableName = os.environ['FOLLOWERS_TABLE_NAME']
 followingTableName = os.environ['FOLLOWING_TABLE_NAME']
+followersBucket = os.environ['FOLLOWERS_BUCKET']
+followingBucket = os.environ['FOLLOWING_BUCKET']
 cacheDuration = os.environ['CACHE_DURATION']
 
 
@@ -33,69 +38,97 @@ def get_overlap(users1, users2):
 
 # -------- DynamoDB Functions --------
 def get_cached_followers(userID):
-    followers_response = dynamodb.get_item(
-        TableName=followersTableName,
-        Key={
-            'UserID': { 'N': userID }
-        },
-    )
-    if 'Item' in followers_response:
-        followers = followers_response['Item']['Followers']['S']
+    try:
+        print(followersBucket)
+        print(userID)
+        data = s3.get_object(Bucket=followersBucket, Key=userID)
+        followers = data.get('Body').read().decode('utf-8')
         return json.loads(followers)
-    else:
+    except ClientError as e:
+        print("ClientError: %s" % e)
         return None
+    # followers_response = dynamodb.get_item(
+    #     TableName=followersTableName,
+    #     Key={
+    #         'UserID': { 'N': userID }
+    #     },
+    # )
+    # if 'Item' in followers_response:
+    #     followers = followers_response['Item']['Followers']['S']
+    #     return json.loads(followers)
+    # else:
+    #     return None
 
 
 def get_cached_following(userID):
-    following_response = dynamodb.get_item(
-        TableName=followingTableName,
-        Key={
-            'UserID': { 'N': userID }
-        },
-    )
-    if 'Item' in following_response:
-        following = following_response['Item']['Following']['S']
+    try:
+        print(followingBucket)
+        print(userID)
+        data = s3.get_object(Bucket=followingBucket, Key=userID)
+        following = data.get('Body').read().decode('utf-8')
         return json.loads(following)
-    else:
+    except ClientError as e:
+        print("ClientError: %s" % e)
         return None
+    # following_response = dynamodb.get_item(
+    #     TableName=followingTableName,
+    #     Key={
+    #         'UserID': { 'N': userID }
+    #     },
+    # )
+    # if 'Item' in following_response:
+    #     following = following_response['Item']['Following']['S']
+    #     return json.loads(following)
+    # else:
+    #     return None
 
 
 def cache_followers(userID, followers):
-    timestamp = int(time.time()) + int(cacheDuration) * 3600
-    cache_response = dynamodb.put_item(
-        TableName=followersTableName,
-        Item={
-            'UserID': {
-                'N': userID
-            },
-            'Followers': {
-                'S': json.dumps(followers)
-            },
-            'ExpireAt': {
-                'N': str(timestamp)
-            }
-        }
+    s3.put_object(
+        Body=json.dumps(followers),
+        Bucket=followersBucket,
+        Key=userID
     )
-    print("DynamoDB Followers response: {}".format(cache_response))
+    # timestamp = int(time.time()) + int(cacheDuration) * 3600
+    # cache_response = dynamodb.put_item(
+    #     TableName=followersTableName,
+    #     Item={
+    #         'UserID': {
+    #             'N': userID
+    #         },
+    #         'Followers': {
+    #             'S': json.dumps(followers)
+    #         },
+    #         'ExpireAt': {
+    #             'N': str(timestamp)
+    #         }
+    #     }
+    # )
+    # print("DynamoDB Followers response: {}".format(cache_response))
 
 
 def cache_following(userID, following):
-    timestamp = int(time.time()) + int(cacheDuration) * 3600
-    cache_response = dynamodb.put_item(
-        TableName=followingTableName,
-        Item={
-            'UserID': {
-                'N': userID
-            },
-            'Following': {
-                'S': json.dumps(following)
-            },
-            'ExpireAt': {
-                'N': str(timestamp)
-            }
-        }
+    s3.put_object(
+        Body=json.dumps(following),
+        Bucket=followingBucket,
+        Key=userID
     )
-    print("DynamoDB Following response: {}".format(cache_response))
+    # timestamp = int(time.time()) + int(cacheDuration) * 3600
+    # cache_response = dynamodb.put_item(
+    #     TableName=followingTableName,
+    #     Item={
+    #         'UserID': {
+    #             'N': userID
+    #         },
+    #         'Following': {
+    #             'S': json.dumps(following)
+    #         },
+    #         'ExpireAt': {
+    #             'N': str(timestamp)
+    #         }
+    #     }
+    # )
+    # print("DynamoDB Following response: {}".format(cache_response))
 
 
 # -------- Main Function (Start) --------
@@ -109,52 +142,80 @@ def followsMyFollowers(event, context):
 
 
 def followsMyFollowersHelper(source_user, target_user):
-    print("{} looking up {}".format(source_user, target_user))
+    try:
+        print("{} looking up {}".format(source_user, target_user))
 
-    # 1. Remove '@' from string (user can input "@handle" or "handle")
-    source_user = source_user.replace("@","")
-    target_user = target_user.replace("@","")
+        # 1. Remove '@' from string (user can input "@handle" or "handle")
+        source_user = source_user.replace("@","")
+        target_user = target_user.replace("@","")
 
-    # 2. Get user info for the source and target users (id, name, profile_url)
-    user_info = get_user_info([source_user, target_user])['data']
-    source_user_info = user_info[0]
-    target_user_info = user_info[1]
-    source_id = source_user_info['id']
-    target_id = target_user_info['id']
+        # 2. Get user info for the source and target users (id, name, profile_url)
+        user_info = get_user_info([source_user, target_user])['data']
+        source_user_info = user_info[0]
+        target_user_info = user_info[1]
+        source_id = source_user_info['id']
+        target_id = target_user_info['id']
 
-    # 3. Get followers of source user (look up in cache first)
-    source_followers = get_cached_followers(source_id)
-    if source_followers is None:
-        source_followers = get_followers_for_id(source_id)
-        cache_followers(source_id, source_followers)
+        # 3. Get followers of source user (look up in cache first)
+        source_followers = get_cached_followers(source_id)
+        if source_followers is None:
+            source_followers = get_followers_for_id(source_id)
+            cache_followers(source_id, source_followers)
+        print("Source followers:")
+        print(len(source_followers))
 
-    # 4. Get users that target follows (look up in cache first)
-    target_following = get_cached_following(target_id)
-    if target_following is None:
-        target_following = get_following_for_id(target_id)
-        cache_following(target_id, target_following)
+        # 4. Get users that target follows (look up in cache first)
+        target_following = get_cached_following(target_id)
+        if target_following is None:
+            target_following = get_following_for_id(target_id)
+            cache_following(target_id, target_following)
+        print("Target followers:")
+        print(len(source_followers))
 
-    # 5. Get users that target follows that follow source
-    users = get_overlap(source_followers, target_following)
+        # 5. Get users that target follows that follow source
+        users = get_overlap(source_followers, target_following)
 
-    # 6. Return response
-    body = {
-        "source": source_user_info,
-        "target": target_user_info,
-        "followers_followed": users,
-    }
+        print("Users:")
+        print(len(users))
 
-    response = {
-        "statusCode": 200,
-        "headers": {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': True,
-        },
-        "body": json.dumps(body, indent=4)
-    }
+        # 6. Return response
+        body = {
+            "source": source_user_info,
+            "target": target_user_info,
+            "followers_followed": users,
+        }
 
-    return response
+        response = {
+            "statusCode": 200,
+            "headers": {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': True,
+            },
+            "body": json.dumps(body, indent=4)
+        }
 
+        return response
+    except Exception as e:
+        if 'Request returned an error: 429' in e:
+            response = {
+                "statusCode": 429,
+                "headers": {
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Credentials': True,
+                },
+                "body": "Too Many Requests"
+            }
+            return response
+        else:
+            response = {
+                "statusCode": 400,
+                "headers": {
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Credentials': True,
+                },
+                "body": "Something went wrong"
+            }
+            return response
 
 # -------- Test Function --------
 
